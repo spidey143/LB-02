082491ee6a1a104ee26130b33810698fbd657288
spidey143 <98106726+spidey143@users.noreply.github.com>
2022-04-30 14:05:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    from math import*
+    print('Для решения уравнения ax^2+bx+c=0')
+    a=float(input('введите коэффицинет a: '))
+    b=float(input('введите коэффицинет b: '))
+    c=float(input('введите коэффицинет c: '))
+    D=b**2-4*a*c
+    print('D=',D,sep='')
+    if D>0:
+        x1=(-b+sqrt(D))/(2*a)
+        x2=(-b-sqrt(D))/(2*a)
+        print('X1=',x1,'\nX2=',x2,sep='')
+    elif D==0:
+        x=(-b) / (2 * a)
+        print('X=',x,sep='')
+    else:
+        print('корней нет')
